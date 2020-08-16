07c0df7d911b8f7bc5b29a0fe5ebe7d5ddde0974
PreciousChicken <hello@preciouschicken.com>
2020-08-16 14:40:43 +0100

    Added API for Vercel

---
+++ client/api/index.js
@@ -0,0 +1,48 @@
+var ethers = require('ethers');
+var CalStore = require('./CalStore.json');
+
+// Ropsten details
+const network = "ropsten";
+const provider = ethers.getDefaultProvider(network, {
+    infura: process.env.INFURA_PROJECT_ID
+});
+
+const contractAddress = process.env.REACT_APP_CALAUTH_ADDRESS;
+
+// Connect to the network
+// We connect to the Contract using a Provider, so we will only
+// have read-only access to the Contract
+let contract = new ethers.Contract(contractAddress, CalStore.abi, provider);
+
+
+// Allows CORS requests when using Vercel deployment
+// IAW https://vercel.com/knowledge/how-to-enable-cors
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another option
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+async function getIcalEvent(userAddress) {
+	let outputString = "";
+	outputString = await contract.getEventsIcal(userAddress);
+	return outputString;
+}
+
+const handler = (req, res) => {
+	const address = "0x0B6ec2E55de5AEf9ED44F8EFf856e428420D8490";
+	getIcalEvent(address).then(cal => {return res.end(cal)});
+}
+
+module.exports = allowCors(handler)
--- client/src/data/ContractAddress.js
+++
@@ -1,2 +0,0 @@
-const CalAuthAddress ='0x69FA228492d2973C07BC6bffFcB48Ce49125625D';
-export default CalAuthAddress;
