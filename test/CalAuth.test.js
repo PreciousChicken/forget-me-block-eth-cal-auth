28188d09ff1c443cdc6c52a5718d3b8a4749478f
PreciousChicken <hello@preciouschicken.com>
2020-08-19 11:30:38 +0100

    Broken tests fixed

--- test/CalAuth.test.js
+++ test/CalAuth.test.js
@@ -1,6 +1,6 @@
 // const { accounts, contract } = require('@openzeppelin/test-environment');
 // const { expect } = require('chai');
-
+const ethers = require('ethers');
 const CalAuth = artifacts.require("CalAuth");
 const truffleAssert = require('truffle-assertions');
 
@@ -15,11 +15,11 @@ contract('CalAuth', async accounts => {
 	it('should not allow non authorised to getEvents', async () => {
 		instance = await CalAuth.deployed();
 		await truffleAssert.reverts(
-			instance.getEventsIcal({from: accounts[1]}),
+			instance.getEventsIcal(accounts[1]),
 			"User not authorised"
 		);
 		await truffleAssert.reverts(
-			instance.getEventsIcal({from: accounts[2]}),
+			instance.getEventsIcal(accounts[2]),
 			"User not authorised"
 		);
 		await truffleAssert.reverts(
@@ -34,7 +34,7 @@ contract('CalAuth', async accounts => {
 
 	it('should allow authorised to blank getEventsIcal', async () => {
 		instance = await CalAuth.deployed();
-		responseIcal = await instance.getEventsIcal({from: accounts[0]});
+		responseIcal = await instance.getEventsIcal(accounts[0]);
 		blankIcal = 'BEGIN:VCALENDAR\n' +
 			'VERSION:2.0\n' +
 			'PRODID:-//preciouschicken.com//forget-me-block-eth-cal\n' +
@@ -49,4 +49,15 @@ contract('CalAuth', async accounts => {
 		blankObj = [];
 		assert.equal(responseObj.length, blankObj.length, "Event Obj unexpected")
 	});
+	
+	it('should not allow read-write add when viewable window reversed', async () => {
+		const USER_WRITE_ROLE = ethers.utils.keccak256(
+			ethers.utils.toUtf8Bytes("USER_WRITE_ROLE"));
+		instance = await CalAuth.deployed();
+		await truffleAssert.reverts(
+			instance.grantRoleAccess(USER_WRITE_ROLE, accounts[1], 1, 0),
+			"Viewable from later than viewable until"
+		);
+	});
+
 });
